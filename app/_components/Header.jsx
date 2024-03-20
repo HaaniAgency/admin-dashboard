802684af296f72a66d1d750d9afc54f70734d056
Taoufiq Jmari <jmaritaoufiq@gmail.com>
2024-03-20 12:03:20 +0000

    Complete

---
+++ app/_components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="flex justify-between px-4 pt-4">
+      <h2 className="bg-gray-600 text-white font-bold rounded-lg px-4 py-2">
+        Welcome Back, Haani
+      </h2>
+    </div>
+  );
+};
+
+export default Header;
